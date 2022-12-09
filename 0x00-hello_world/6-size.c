9a6c0b23109011e1147073f1b01e9f45fd79dc21
tobidinho10 <oluwatobi.ikuesan@gmail.com>
2022-12-08 23:02:43 -0800

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-gcc 6-size.c -o 6-size
+#include <stdio.h>
+
+/**
+*main -Entry point
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(S)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	print("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
