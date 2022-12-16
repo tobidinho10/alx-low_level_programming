d8758e8ce791f101299b3e142dea1f0f6707f701
tobidinho10 <oluwatobi.ikuesan@gmail.com>
2022-12-15 20:00:10 -0800

    more functions nested

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include <main.h>
 /**
- * _supper - A function that checks for uppercase character
+ * _isupper - A function that checks for uppercase character
  * @c: An input character
  * Return: 1 if c is uppercase or 0 otherwise
  */
