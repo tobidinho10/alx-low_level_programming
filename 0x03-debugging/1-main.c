426a8deba17b32eee0fea0acb1968c910ef49532
tobidinho10 <oluwatobi.ikuesan@gmail.com>
2022-12-16 19:16:38 -0800

    1 TASK

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int i;
+
+printf("infinite loop incoming :(\n");
+
+i = 0;
+/*
+*while (i < 10)
+*{
+*
+*}
+*/
+printf("infinite loop avoided! \\o/\n");
+
+return (0);
