f8d5c23a2461e2dd4e6fb9492ffcc90edb170438
tobidinho10 <oluwatobi.ikuesan@gmail.com>
2022-12-19 17:11:51 +0000

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -30,19 +30,28 @@ int main(void)
 		    diff_half1++;
 
 
-	for (index = 0; passwoed[index]; index++)
+	for (index = 0; password[index]; index++)
 	{  
 		if (password[index] >= (33 + diff_half1))
 		{
-			password[index] -= diff_half;
+			password[index] -= diff_half1;
 			break;
 		}
 	}
-     }
+	for (index = 0; password[index]; index++)
+	{
+		if (password[index] >= (33 + diff_half2))
+	        {
+			password[index] -= diff_half2;
+			break;
+		}
+	    }
+	}
+     
 
-     printf("%s", password;
+     printf("%s", password);
 
-     return (0)
+     return (0);
 }
 
 
