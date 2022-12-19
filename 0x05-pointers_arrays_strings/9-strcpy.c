0694a60bdfb56690cd1121cd6e4e4cff7ae438d6
tobidinho10 <oluwatobi.ikuesan@gmail.com>
2022-12-19 15:47:15 +0000

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _stecpy - copy the string pointed to by src to desk
+ * @dest: char to check
+ * @src: char to check
+ * Return: 0 is success
+ */
+char *_strpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = ; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+	return (dest);
+}
+
