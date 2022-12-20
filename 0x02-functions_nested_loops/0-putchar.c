b48c8179054ae350422d32cda7ffe20819124cf0
tobidinho10 <oluwatobi.ikuesan@gmail.com>
2022-12-20 05:53:47 +0000

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print _putcher - writes the character c to stdout
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putcher('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
