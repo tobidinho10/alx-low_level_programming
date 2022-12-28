93fd7f72483816e0fd8dfaeafa40b4f23a59aa82
tobidinho10 <oluwatobi.ikuesan@gmail.com>
2022-12-28 04:32:49 -0800

    done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,32 @@
 #include "main.h"
 
+
+
+
+
 /**
- * main - Test function for position or negative
+
+ * main - tests function
+
  * Return: 0
+
  */
 
+
+
 int main(void)
+  
 {
-int i;
+  
+  int i;
+  
 
-i = 0;
-positive_or_negative(i);
+  
+  i = 0;
+  
+  positive_or_negative(i);
+  
+  return (0);
+  
+}
 
-return (0);
