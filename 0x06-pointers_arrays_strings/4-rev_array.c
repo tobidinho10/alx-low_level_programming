afdc50c506d7921eaecfe96cff35d077bfc25f06
tobidinho10 <oluwatobi.ikuesan@gmail.com>
2022-12-28 05:01:19 -0800

    done

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,18 +1,36 @@
 #include "main.h"
-/**
- * reverse_array - reverse array
- * @a:array
- * @n:integer
- * Retuen:void
- */
-void reverse_array(int *a, int n)
-{
-int i, c;
 
-for (i = 0; (i < (n - 1) / 2); i++)
+/**
+
+ * reverse_array - reverse array
+
+ * @a:array
+
+ * @n:integer
+
+ * Return:void
+
+ */
+
+void reverse_array(int *a, int n)
+  
+{
+  
+  int i, c;
+  
+
+  
+  for (i = 0; (i < (n - 1) / 2); i++)
+    
     {
-    c = a[i];
-    a[i] = a[n - 1 i];
-    a[n - 1 - i] = c;
+      
+      c = a[i];
+      
+      a[i] = a[n - 1 - i];
+      
+      a[n - 1 - i] = c;
+      
     }
+  
 }
+
